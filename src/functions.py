ffe4c7a3867396907870cb827d52b2b68557573d
sekhepri <jonas@jonas.as>
2025-08-19 15:54:05 +0200

    added functions for extracting image and links from markdown

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from htmlnode import *
 from textnode import *
+import re
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
@@ -39,3 +40,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 )
             use_text_type = not use_text_type
     return new_nodes
+
+#Boot.dev recomends r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+def extract_markdown_images(text):
+    alt_text = re.findall(r"!\[(.*?)\]",text)
+    src_img = re.findall(r"\((.*?)\)", text)
+    return list(zip(alt_text, src_img))
+
+#Boot.dev recomends: r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+def extract_markdown_links(text):
+    link_texst = re.findall(r"\[(.*?)\]",text)
+    src_link = re.findall(r"\((.*?)\)", text) #
+    return list(zip(link_texst, src_link))
